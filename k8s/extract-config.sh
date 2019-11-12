a4a017ae25f4bc5dba041fcd5aae0ceb35ad8d00
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-11-11 17:52:08 -0800

    reversed directory naming

--- k8s/extract-config.sh
+++ k8s/extract-config.sh
@@ -14,8 +14,15 @@ kubectl api-resources -o name | while read resource; do
     continue;
   fi
   kubectl get $resource --all-namespaces -o go-template --template='{{range .items}}{{if .metadata.namespace}}{{.metadata.namespace}}{{else}}_{{end}} {{.metadata.name}}{{"\n"}}{{end}}' | while IFS=" " read -r namespace name; do
-    mkdir -p $resource
-    yamlfile="$resource/$namespace.$name.yaml"
+    IFS='.' read -r -a array <<< "$resource"
+    dirname="${array[n-1]}"
+    n=${#array[*]}
+    for (( i = n-2; i >= 0; i-- ))
+    do
+        dirname="${dirname}.${array[i]}"
+    done
+    mkdir -p $dirname
+    yamlfile="$dirname/$namespace.$name.yaml"
     if [ $namespace = "_" ]; then nsflag=""; else nsflag="-n $namespace"; fi
     start_job "kubectl get $resource $name $nsflag -o yaml > $yamlfile"
   done
